7768b3039ed4b7fa9e085ed88444883f0816931c
geops22 <geops22@gmail.com>
2022-05-28 17:32:54 -0500

    initial release

---
+++ config.py
@@ -0,0 +1,23 @@
+# List of disallowed delegates
+disallowed = ['arky', 'biz_classic', 'biz_private']
+
+# Additional restrictions for selecting delegates based on min/max share percent and payout interval
+min_share = 70
+max_share = 99
+min_time = 1
+max_time = 24
+
+# Reward gain to trigger a recommendation, in number of coins
+gain = 0.1
+
+# Dictionary of addresses and their names in the format 'name':'address'
+addresses = {
+	'name1':'address1',
+        'name2':'address2'
+}
+
+# Core API
+api = 'https://api.ark.io/api'
+
+# Delegate Share API
+sapi = 'https://arkdelegates.live/api/delegates?limit=100'
